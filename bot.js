00138215633874b3e7d5d48360e9bc3c1a74239e
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-22 11:55:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2175,30 +2175,7 @@ client.on("ready", () => {
     });
 
 });
-const devs = ['351366504068939777'];
-          const adminprefix = ".";
-          client.on('message', message => {
-              var argresult = message.content.split(`).slice(1).join(' ');
-                if (!devs.includes(message.author.id)) return;
 
-            if (message.content.startsWith(adminprefix + 'ply')) {
-              client.user.setGame(argresult);
-                message.channel.sendMessage(:white_check_mark:   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'wt')) {
-            client.user.setActivity(argresult, {type:'WATCHING'});
-                message.channel.sendMessage(:white_check_mark:   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'ls')) {
-            client.user.setActivity(argresult , {type:'LISTENING'});
-                message.channel.sendMessage(:white_check_mark:   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'st')) {
-              client.user.setGame(argresult, "https://www.twitch.tv/ حب ");
-                message.channel.sendMessage(:white_check_mark:   ${argresult}`)
-            }
-            });
-});
 const bannedwords = [
     "كسمك",
     "يابن المتناكه",
