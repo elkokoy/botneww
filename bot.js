0911052084edbd8ca8fc8dee60b630d9b423f2ba
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-25 14:47:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2307,7 +2307,7 @@ client.on("message", (message) => {
     /// DREAM
    if (message.content.startsWith("-new")) {     /// DREAM
         const reason = message.content.split(" ").slice(1).join(" ");     /// DREAM
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
+        if (!message.guild.roles.exists("name", "• Nova » Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`• Nova » Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "• Nova » Support Team");
